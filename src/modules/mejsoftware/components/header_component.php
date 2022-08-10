54f77f0b49f3b9b7e472d26c241b00df0ce1ae6e
GungryNutria <edgardochamorro08@gmail.com>
2022-08-09 21:08:45 -0500

    Botones Estilizados

--- src/modules/mejsoftware/components/header_component.php
+++ src/modules/mejsoftware/components/header_component.php
@@ -7,10 +7,25 @@
         <div class="header-body-content">
             <h1>CREATIVA Y MINIMALISTA</h1>
             <h2>AGENCIA DE SOFTWARE</h2>
-        </div>
-        <div class="hero-image">
-            <img src="src/assets/hero-image.jpg" alt="">
+            <br>
+            <p>Creemos que cualquier empresa puede entrar a el mundo digital, la tecnología es una herramienta que debe estar al alcance de toda empresa.</p>
+            <br>
+            <div class="buttons">
+                <div class="primary-button">
+                    <div class="top-line"></div>
+                    <div class="bottom-line"></div>
+                    <a href="#">Boton 1</a>
+                </div>
+                <div class="secundary-button">
+                    <div class="top-line"></div>
+                    <div class="bottom-line"></div>
+                    <a href="#">Boton 2</a>
+                </div>
+            </div>
         </div>
     </div>
+    <div class="hero-image">
+            <img src="src/assets/hero-image.jpg" alt="">
+        </div>
 </header>
 </center>
